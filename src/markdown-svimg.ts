07603cfd65b2919ad8adda8a340db8d8dc74d8c3
Chris Han <christopher.f.han@gmail.com>
2020-05-27 17:47:49 -0500

    Update to new queue, factor out rehype plugin

--- src/markdown-svimg.ts
+++ src/markdown-svimg.ts
@@ -1,35 +1,15 @@
 import globby from 'globby';
-import unified, { Transformer } from 'unified';
+import unified from 'unified';
 import markdown from 'remark-parse';
 import remark2rehype from 'remark-rehype';
 import raw from 'rehype-raw';
 import html from 'rehype-stringify';
 import fs from 'fs';
 import matter from 'gray-matter';
-import { ImageProcessingQueue } from 'svimg/dist/process';
+import { Queue, processImage } from 'svimg/dist/process';
 import { join, dirname } from 'path';
-import { Node } from 'unist';
-import visit from 'unist-util-visit';
-
-interface ImageNode {
-    type: 'element';
-    tagName: string;
-    properties: {
-        [attr: string]: string;
-    },
-}
-
-// TODO: import RehypeSvimgOptions from rehype-svimg
-interface RehypeSvimgOptions {
-    inputDir: string;
-    outputDir: string;
-    webp?: boolean;
-    width?: number;
-}
-
-interface RehypePluginOptions extends RehypeSvimgOptions {
-    processingQueue: ImageProcessingQueue;
-}
+import { RehypeSvimgOptions } from 'rehype-svimg/dist/rehype-svimg';
+import rehypeSvimgProcess from './rehype-svimg-process';
 
 interface MarkdownSvimgOptions {
     files: string | string[];
@@ -38,64 +18,25 @@ interface MarkdownSvimgOptions {
     rehypeOptions: RehypeSvimgOptions | ((input: { file: string }) => RehypeSvimgOptions)
 }
 
-function rehypePlugin(options: RehypePluginOptions): Transformer {
-
-    return async function transformer(tree, file): Promise<Node> {
-        const imageNodes: ImageNode[] = [];
-
-        visit(tree, { type: 'element', tagName: 'img' }, (node: Node) => {
-            imageNodes.push(node as any as ImageNode);
-        });
-
-        for (const node of imageNodes) {
-            if (!(node.properties && node.properties.src)) {
-                continue;
-            }
-
-            const src = node.properties.src;
-
-            let width: number | undefined;
-            if (node.properties.width) {
-                if (/^[0-9]+$/.test(node.properties.width)) {
-                    width = parseInt(node.properties.width, 10);
-                }
-            } else if (options?.width) {
-                width = options.width;
-            }
-
-            await options.processingQueue.process({
-                inputFile: join(options.inputDir, src),
-                outputDir: join(options.outputDir, dirname(src)),
-                options: {
-                    webp: options.webp,
-                    widths: width ? [width] : undefined,
-                }
-            });
-        }
-
-        return tree;
-    }
-}
-
 export default function markdownSvimg(options: MarkdownSvimgOptions) {
     if (!(options?.files && options.files.length)) {
         throw new Error('Files are required');
     }
 
-    const processingQueue = new ImageProcessingQueue();
+    const queue = new Queue();
 
     return {
         name: 'markdown-svimg',
         buildEnd: async () => {
+
             const markdownFiles = await globby(options.files);
 
             if (!markdownFiles.length) {
                 return;
             }
 
-            for (const file of markdownFiles) {
-
-                const fileContent = await fs.promises.readFile(file);
+            await Promise.all(markdownFiles.map(async (file) => {
+                const fileContent = await queue.enqueue(fs.promises.readFile, file);
                 const data = matter(fileContent);
 
                 const opts = typeof options.rehypeOptions === 'function' ? options.rehypeOptions({ file }) : options.rehypeOptions;
@@ -106,30 +47,36 @@ export default function markdownSvimg(options: MarkdownSvimgOptions) {
                 } else {
                     processor = processor.use(remark2rehype).use(html);
                 }
-                processor = processor.use(rehypePlugin, {
+                processor = processor.use(rehypeSvimgProcess, {
                     ...opts,
-                    processingQueue
+                    queue,
                 });
 
-                await processor.process(data.content);
+                const promises: Array<Promise<any>> = [
+                    processor.process(data.content),
+                    ...(options?.frontMatterImageKeys?.length ?
+                        options.frontMatterImageKeys.map(async (key) => {
+                            const image = data.data[key];
 
-                if (options?.frontMatterImageKeys?.length) {
-                    for (const key of options.frontMatterImageKeys) {
-                        const image = data.data[key];
-                        if (image) {
-                            await processingQueue.process({
-                                inputFile: join(opts.inputDir, image),
-                                outputDir: join(opts.outputDir, dirname(image)),
-                                options: {
+                            if (!image) {
+                                return;
+                            }
+
+                            await processImage(
+                                join(opts.inputDir, image),
+                                join(opts.outputDir, dirname(image)),
+                                queue,
+                                {
                                     webp: opts.webp,
                                     widths: opts && opts.width ? [opts.width] : undefined,
                                     skipGeneration: false,
                                 }
-                            });
-                        }
-                    }
-                }
-            }
+                            );
+                        }) : [])
+                ];
+
+                await Promise.all(promises);
+            }));
         }
     };
 }
---
+++ src/rehype-svimg-process.ts
@@ -0,0 +1,57 @@
+import { RehypeSvimgOptions } from 'rehype-svimg/dist/rehype-svimg';
+import { Queue, processImage } from 'svimg/dist/process';
+import { Transformer } from 'unified';
+import { Node } from 'unist';
+import visit from 'unist-util-visit';
+import { join, dirname } from 'path';
+
+export interface RehypeSvimgProcessOptions extends RehypeSvimgOptions {
+    queue: Queue;
+}
+
+interface ImageNode {
+    type: 'element';
+    tagName: string;
+    properties: {
+        [attr: string]: string;
+    },
+}
+
+export default function rehypeSvimgProcess(options: RehypeSvimgProcessOptions): Transformer {
+    return async function transformer(tree, file): Promise<Node> {
+        const imageNodes: ImageNode[] = [];
+
+        visit(tree, { type: 'element', tagName: 'img' }, (node: Node) => {
+            imageNodes.push(node as any as ImageNode);
+        });
+
+        await Promise.all(imageNodes.map(async (node) => {
+            if (!(node.properties && node.properties.src)) {
+                return;
+            }
+
+            const src = node.properties.src;
+
+            let width: number | undefined;
+            if (node.properties.width) {
+                if (/^[0-9]+$/.test(node.properties.width)) {
+                    width = parseInt(node.properties.width, 10);
+                }
+            } else if (options?.width) {
+                width = options.width;
+            }
+
+            await processImage(
+                join(options.inputDir, src),
+                join(options.outputDir, dirname(src)),
+                options.queue,
+                {
+                    webp: options.webp,
+                    widths: width ? [width] : undefined,
+                }
+            );
+        }));
+
+        return tree;
+    }
+}
